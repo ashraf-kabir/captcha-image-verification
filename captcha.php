7ce7f999850afe91ceef848735bbf225a5647bff
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-02-07 11:37:06 +0600

    Update captcha.php

--- captcha.php
+++ captcha.php
@@ -5,7 +5,7 @@ $_SESSION['vcode'] = $text;
 $height = 25;
 $width = 60;
 $img_p = imagecreate($width, $height);
-$black = imagecolorallocate($img_p, 0, 0, 0);
+$background = imagecolorallocate($img_p, 98, 3, 252);
 $white = imagecolorallocate($img_p, 255, 255, 255);
 $font_size = 14;
 imagestring($img_p, $font_size, 5, 5, $text, $white);
